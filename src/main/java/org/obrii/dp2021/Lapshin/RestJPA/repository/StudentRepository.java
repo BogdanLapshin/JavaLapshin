654a4a6fe803f40e2ad666e194dc223593b91905
BogdanLapshin <bogdanizizmaila@gmail.com>
2021-05-27 16:17:10 +0300

    Initial commit

---
+++ src/main/java/org/obrii/dp2021/Lapshin/RestJPA/repository/StudentRepository.java
@@ -0,0 +1,14 @@
+
+package org.obrii.dp2021.Lapshin.RestJPA.repository;
+
+import java.util.List;
+import org.obrii.dp2021.Lapshin.RestJPA.entity.Student;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource(collectionResourceRel="student", path="student")
+public interface StudentRepository extends PagingAndSortingRepository<Student,Long>{
+    List<Student> findByName(@Param("name") String name);
+}
